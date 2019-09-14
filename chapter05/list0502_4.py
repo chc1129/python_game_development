a88a3e55ca648b04d65fc22fc232715a95f8c609
chicu <chc1129@gmail.com>
2019-09-14 18:22:35 +0900

    new file commit

---
+++ chapter05/list0502_4.py
@@ -0,0 +1,13 @@
+QUESSTION = [
+"サザエさんの旦那さんの名前は？",
+"カツオの妹の名前は？",
+"タラちゃんはカツオからみてどんな関係？"]
+R_ANS = ["マスオ", "ワカメ", "甥"]
+R_ANS2 = ["ますお", "わかめ", "おい"]
+for i in range(3):
+    print(QUESSTION[i])
+    ans = input()
+    if ans == R_ANS[i] or ans == R_ANS2[i]:
+        print("正解です")
+    else:
+        print("不正解です")
